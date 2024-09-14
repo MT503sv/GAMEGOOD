2131a71449441b346bd25df369591b04ea2376d2
MINEDUCYT <mateoantoniomenjivarmelara@gmail.com>
2024-09-13 21:14:57 -0600

    GAMEGOOD

---
+++ index.js
@@ -0,0 +1,18 @@
+const GameViewport = {
+    WIDTH: 384,
+    HEIGHT: 224,
+    SCALE: 4,
+};
+
+window.onload = function () {
+    const canvasEL = document.querySelector('canvas');
+    const contexto = canvasEL.getContext('2d');
+
+    canvasEL.width = GameViewport.WIDTH;
+    canvasEL.height = GameViewport.HEIGHT;
+    
+    canvasEL.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
+    canvasEL.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+
+    console.log(context);
+}
