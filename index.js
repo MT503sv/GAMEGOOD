229c10125d741c5f47193024365edf5a45660a69
MINEDUCYT <mateoantoniomenjivarmelara@gmail.com>
2024-09-14 18:42:44 -0600

    chido

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const GameViewport = {
-    WIDTH: 384 / 3,
-    HEIGHT: 224 / 3,
+    WIDTH: 384  ,
+    HEIGHT: 224 ,
 };
 
 window.onload = function () {
@@ -10,16 +10,24 @@ window.onload = function () {
     canvasEL.width = GameViewport.WIDTH;
     canvasEL.height = GameViewport.HEIGHT;
 
-    const CAP = document.querySelector('img');
+    const ken = document.querySelector('img');
 
-    context.strokeStyle = `yellow`;
-    context.moveTo(0, 0);
-    context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
-    context.moveTo(GameViewport.WIDTH, 0);
-    context.lineTo(0 ,GameViewport.HEIGHT);
-    context.stroke();
+    const position =
 
-    context.drawImage(CAP, 0, 0);
+    function frame(){
+        context.strokeStyle = `yellow`;
+        context.moveTo(0, 0);
+        context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+        context.moveTo(GameViewport.WIDTH, 0);
+        context.lineTo(0 ,GameViewport.HEIGHT);
+        context.stroke();
+
+        context.drawImage(ken, 0, 0);
+
+        window.requestAnimationFrame(frame);
+    }
+
+    window.requestAnimationFrame(frame);
 
     console.log(context);
-}
+    }
